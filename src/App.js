c48fdde7f959d81c59458a82193d60faaaa49e0c
Franco Nicolás Placidi <79880040+francoplacidi@users.noreply.github.com>
2022-06-02 09:19:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
                     <p className="field">Password:</p>
                     <input type="password" />
                     <br />
-                    <a data-qa="login-menu" id="get-into" href="/login" id="sign-in" class="text-warning border-radius-round border-h-dark ml-3 pl-20 pr-20 p-0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#1D1E20" width="24px" height="24px"><g fill="none"><path d="M0 0h24v24H0V0z"></path><path d="M0 0h24v24H0V0z" opacity=".87"></path></g><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path></svg> <p class="fs-15 ml-10 fw-600 fs-17-xxl">Login</p></a>
+                    <a data-qa="login-menu" id="get-into" href="#" id="sign-in" class="text-warning border-radius-round border-h-dark ml-3 pl-20 pr-20 p-0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#1D1E20" width="24px" height="24px"><g fill="none"><path d="M0 0h24v24H0V0z"></path><path d="M0 0h24v24H0V0z" opacity=".87"></path></g><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path></svg> <p class="fs-15 ml-10 fw-600 fs-17-xxl">Login</p></a>
                   </form>
                   <div>
                     <p className="link">
